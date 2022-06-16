618029c29832399efc0c1f23534d7c8975b5aeb4
lucy1224 <87363664+riyarei@users.noreply.github.com>
2022-06-16 15:00:52 +0800

    merge html and php into  php

--- change_psw_p.php
+++ change_psw_p.php
@@ -3,77 +3,123 @@ session_start();
 require_once('connect_db.php');
 
 $login_p_id = $_SESSION['player_id'];
+?>
 
+<!DOCTYPE html>
+<html>
+<head>
+	<title>線上扭蛋機</title>
+		<meta http-equiv="Content-Type" content="text/css; charset=utf-8">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h3>更改密碼、地址、儲值金幣</h3>
 
-if ( isset($_POST['new_psw'])  ) {
-	$get_new_addr = '';
-	$get_save_money = '';
-    //echo $login_p_id."<br>";
-    
-	//$old_psw = $_POST['old_psw'];
-	$new_psw = $_POST['new_psw'];
-    //echo $new_psw."<br>";
+    <form action="change_psw_p.php" method="post">
+        <div>
+            新密碼<input type="text" name="new_psw" />
+            <input type="submit" name='submit_1' value="更改密碼">
+        </div>
+    </form>
+    <form action="change_psw_p.php" method="post">
+        <div>
+            新地址<input type="text" name="new_addr" />
+            <input type="submit" name='submit_2' value="更改地址">
+        </div>
+    </form>
+    <form action="change_psw_p.php" method="post">
+        <div>
+            儲值金幣<input type="text" name="save_money" />
+            <input type="submit" name='submit_3'  value="儲值">
+        </div>
+    </form>
+   
+    <form action="phome.php"><input type="submit"  value="返回"></form>
+   
+</body>
+</html>
+<?php
 
-    $sql_1 = "UPDATE player SET password='$new_psw' WHERE player_id = '$login_p_id'";
-	$change_psw_sql = mysqli_query($conn, $sql_1);
-	echo $change_psw_sql."<br>";
-
-    $result1 = $conn->query($change_psw_sql);
-    echo $result1."<br>";
-	if ( ($result1 ) === TRUE) {
-        echo "<div align='center'> <h2><font color='antiquewith'>舊密碼!!</font></h2> <h3><a href='change_psw_p.html'>返回</a></h3> </div>";
+if(isset($_POST['submit_1'])){
+	if ( isset($_POST['new_psw']) && !empty($_POST['new_psw'])) {
+		//echo $login_p_id."<br>";
 		
-	} else {
-		echo "<div align='center'> <h2><font color='antiquewith'>密碼更改成功!!</font></h2> <h3><a href='phome.php'>返回首頁</a></h3> </div>";
+		//$old_psw = $_POST['old_psw'];
+		$new_psw = $_POST['new_psw'];
+		//echo $new_psw."<br>";
+	
+		$sql_1 = "UPDATE player SET password='$new_psw' WHERE player_id = '$login_p_id'";
+		$change_psw_sql = mysqli_query($conn, $sql_1);
+		//echo $change_psw_sql."<br>";
+	
+		$result1 = $conn->query($change_psw_sql);
+		//echo $result1."<br>";
+		if ( ($result1 ) === TRUE) {
+			echo "<div align='center'> <h2><font color='antiquewith'>有錯誤，請重試!!</font></h2> <h3><a href='change_psw_p.html'>返回</a></h3> </div>";
+			
+		} else {
+			echo "<div align='center'> <h2><font color='antiquewith'>密碼更改成功!!</font></h2> <h3><a href='phome.php'>返回首頁</a></h3> </div>";
+		}
+		exit();
+	}else if(empty($_POST['new_psw'])){
+		$message = '請填入新密碼後再送出!';
+        echo "<script type='text/javascript'>alert('$message');</script>";
 	}
-	exit();
 }
 
-if ( isset($_POST['new_addr'])  ) {
-	$get_new_psw = '';
-	$get_save_money = '';
-	//$old_psw = $_POST['old_psw'];
-	$new_addr = $_POST['new_addr'];
-    //echo $new_psw."<br>";
+else if (isset($_POST['submit_2'])){
 
-    $sql_2 = "UPDATE player SET address='$new_addr' WHERE player_id = '$login_p_id'";
-	$change_addr_sql = mysqli_query($conn, $sql_2);
-	//echo $change_psw_sql."<br>";
-
-    $result2 = $conn->query($change_addr_sql);
-    echo $result2."<br>";
-	if ( ($result2 ) === TRUE) {
-        echo "<div align='center'> <h2><font color='antiquewith'>!!</font></h2> <h3><a href='change_psw_p.html'>返回</a></h3> </div>";
-		
-	} else {
-		echo "<div align='center'> <h2><font color='antiquewith'>地址更改成功!!</font></h2> <h3><a href='phome.php'>返回首頁</a></h3> </div>";
+	if ( isset($_POST['new_addr']) && !empty($_POST['new_addr']) ) {
+		//$old_psw = $_POST['old_psw'];
+		$new_addr = $_POST['new_addr'];
+		//echo $new_psw."<br>";
+	
+		$sql_2 = "UPDATE player SET address='$new_addr' WHERE player_id = '$login_p_id'";
+		$change_addr_sql = mysqli_query($conn, $sql_2);
+		//echo $change_psw_sql."<br>";
+	
+		$result2 = $conn->query($change_addr_sql);
+		//echo $result2."<br>";
+		if ( ($result2 ) === TRUE) {
+			echo "<div align='center'> <h2><font color='antiquewith'>有錯誤，請重試!!</font></h2> <h3><a href='change_psw_p.html'>返回</a></h3> </div>";
+			
+		} else {
+			echo "<div align='center'> <h2><font color='antiquewith'>地址更改成功!!</font></h2> <h3><a href='phome.php'>返回首頁</a></h3> </div>";
+		}
+		exit();
+	}else if (empty($_POST['new_addr'])){
+		$message = '請填入新地址後再送出!';
+        echo "<script type='text/javascript'>alert('$message');</script>";
 	}
-	exit();
+}
+else if (isset($_POST['submit_3'])){
+
+	if ( isset($_POST['save_money']) && !empty($_POST['save_money'])  ) {
+		//$old_psw = $_POST['old_psw'];
+		$save_money = $_POST['save_money'];
+		//echo $new_psw."<br>";
+	
+		$sql_3 = "UPDATE player SET money = money + '$save_money' WHERE player_id = '$login_p_id'";
+		$save_money_sql = mysqli_query($conn, $sql_3);
+		//echo $change_psw_sql."<br>";
+	
+		$result3 = $conn->query($save_money_sql);
+		//echo $result3."<br>";
+		if ( ($result3 ) === TRUE) {
+			echo "<div align='center'> <h2><font color='antiquewith'>有錯誤，請重試!!</font></h2> <h3><a href='change_psw_p.html'>返回</a></h3> </div>";
+			
+		} else {
+			echo "<div align='center'> <h2><font color='antiquewith'>已儲值!!</font></h2> <h3><a href='phome.php'>返回首頁</a></h3> </div>";
+		}
+		exit();
+	}else{
+		//echo "資料不完全";
+		$message = '請填入儲值金額後再送出!';
+       		echo "<script type='text/javascript'>alert('$message');</script>";
+	}
 }
 
-if ( isset($_POST['save_money'])  ) {
-	$get_new_psw = '';
-	$get_new_addr = '';
-	//$old_psw = $_POST['old_psw'];
-	$save_money = $_POST['save_money'];
-    //echo $new_psw."<br>";
 
-    $sql_3 = "UPDATE player SET money = money + '$save_money' WHERE player_id = '$login_p_id'";
-	$save_money_sql = mysqli_query($conn, $sql_3);
-	//echo $change_psw_sql."<br>";
-
-    $result3 = $conn->query($save_money_sql);
-    echo $result3."<br>";
-	if ( ($result3 ) === TRUE) {
-        echo "<div align='center'> <h2><font color='antiquewith'>舊密碼!!</font></h2> <h3><a href='change_psw_p.html'>返回</a></h3> </div>";
-		
-	} else {
-		echo "<div align='center'> <h2><font color='antiquewith'>已儲值!!</font></h2> <h3><a href='phome.php'>返回首頁</a></h3> </div>";
-	}
-	exit();
-}else{
-	echo "資料不完全";
-}
 
 
 ?>
