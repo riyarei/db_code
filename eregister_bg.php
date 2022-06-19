73f427ea1ff563be20f9dcf163d5fa69c3ec31db
rmw0225 <71578827+rmw0225@users.noreply.github.com>
2022-06-19 16:40:04 +0800

    echo  註冊成功!

--- eregister_bg.php
+++ eregister_bg.php
@@ -24,7 +24,7 @@ else if ( !empty($_POST['password']) && !empty($_POST['account'])  ) {
 	$insert_sql = mysqli_query($conn, "INSERT INTO enterprise(password, account, money) VALUES('$psw', '$account', 0)");	// 在 player 表新增資料 
 	
 	if ($conn->query($insert_sql) !== TRUE) {
-		echo "註冊成功!!<br> <a href='elogin.html'>返回登入</a>";
+		echo "<div align='center'> <h2><font color='black'>註冊成功!!<br></h2> <h3><a href='elogin.html'>返回登入</a></h3></div>";
 	} else {
 		echo "<div align='center'> <h2><font color='antiquewith'>註冊失敗!!</font></h2> <h3><a href='eregister.php'>返回</a></h3> </div>";
 	}
